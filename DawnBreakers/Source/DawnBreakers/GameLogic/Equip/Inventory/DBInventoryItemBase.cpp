a34d7a748c287d2609dc2656672755741feb53e1
seebro <954818696@qq.com>
2016-12-29 14:29:00 +0800

    14:28

---
+++ DawnBreakers/Source/DawnBreakers/GameLogic/Equip/Inventory/DBInventoryItemBase.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DawnBreakers.h"
+#include "DBInventoryItemBase.h"
+
+
+// Sets default values
+ADBInventoryItemBase::ADBInventoryItemBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADBInventoryItemBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADBInventoryItemBase::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ DawnBreakers/Source/DawnBreakers/GameLogic/Equip/Weapon/DBWeaponBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameLogic/Equip/Inventory/DBInventoryItemBase.h"
+#include "DBWeaponBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAWNBREAKERS_API ADBWeaponBase : public ADBInventoryItemBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
