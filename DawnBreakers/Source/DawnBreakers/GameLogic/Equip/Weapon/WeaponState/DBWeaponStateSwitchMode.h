c5f528b124772e2f64c2cd739330814e7ea5a8a4
seebro <954818696@qq.com>
2016-12-30 11:19:26 +0800

    NF 新增武器模式切换状态

---
+++ DawnBreakers/Source/DawnBreakers/GameLogic/Equip/Weapon/WeaponState/DBWeaponStateSwitchMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameLogic/Equip/Weapon/WeaponState/DBWeaponStateBase.h"
+#include "DBWeaponStateSwitchMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DAWNBREAKERS_API UDBWeaponStateSwitchMode : public UDBWeaponStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
